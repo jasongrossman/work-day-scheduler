6224eb8d7ef440b944fdbdd0af3e83ce3e7eaf8f
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-07-22 13:16:41 -0400

    added time blocks and formatted

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,4 +1,18 @@
+
+//Display current date/time in Jumbotron
 var now = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-
-
 var todaysDate = $("#currentDay").text(now);
+var timeBlocksCounter = 8;
+var beginDay = moment().set("hour", 9).set("minute", 0);
+
+//load calendar skeleton on page load
+var loadCalendar = function() {
+
+}
+
+    //retrieve data from local storage
+
+    //add any local storage data into time blocks    
+
+// timeBlockOptions();
+loadCalendar();
